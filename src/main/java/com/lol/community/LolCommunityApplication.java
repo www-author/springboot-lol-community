c001a50396db17177ca0e2085f5de25bd5da88e0
www-author <dream970215@naver.com>
2024-03-25 13:19:11 +0900

    Initial commit

---
+++ src/main/java/com/lol/community/LolCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.lol.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LolCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LolCommunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lol/community/LolCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lol.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LolCommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
