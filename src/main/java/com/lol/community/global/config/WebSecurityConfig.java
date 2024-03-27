221703a842e608653528aee1c183729ca830571d
www-author <dream970215@naver.com>
2024-03-27 13:45:15 +0900

    fix : h2 콘솔 제외

--- src/main/java/com/lol/community/global/config/WebSecurityConfig.java
+++ src/main/java/com/lol/community/global/config/WebSecurityConfig.java
@@ -13,8 +13,6 @@ public class WebSecurityConfig {
     @Bean // 특정 대상 스프링 시큐리티 기능 비활성화
     public WebSecurityCustomizer configure() {
         return web -> web.ignoring()
-                .requestMatchers(toH2Console())
-                // 스웨거도  security 비활성화 추가
                 .requestMatchers("/static/**","/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html");
     }
 }
