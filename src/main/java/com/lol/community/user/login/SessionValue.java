62ef2714e40b13f7839f029580b03ae0ebf5f84f
HoJun <sfy0415@gmail.com>
2024-04-03 11:23:44 +0900

    feat: 회원 정보 수정 기능 추가

---
+++ src/main/java/com/lol/community/user/login/SessionValue.java
@@ -0,0 +1,12 @@
+package com.lol.community.user.login;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@AllArgsConstructor
+public class SessionValue {
+    private Integer userId;
+    private String grade;
+}
