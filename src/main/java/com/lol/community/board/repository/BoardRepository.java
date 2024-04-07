f464b0848297c90e78f045a196d61ce9d51ac2bc
www-author <dream970215@naver.com>
2024-04-08 00:27:52 +0900

    fix : 사용자 게시판 권한 조회 로직 수정

--- src/main/java/com/lol/community/board/repository/BoardRepository.java
+++ src/main/java/com/lol/community/board/repository/BoardRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -20,4 +22,33 @@ public interface BoardRepository extends JpaRepository<Board, Integer> {
     );
 
     List<Board> findAllByBoardTypeAndIsDeletedIsFalseOrderByViewCountDesc(String boardType, PageRequest pageRequest);
+
+    @Query(
+            "select board " +
+            "from Board board " +
+            "join fetch board.user " +
+            "where board.isDeleted = false " +
+            "and board.boardType = :boardType " +
+            "and board.user.grade in(:grades) "
+    )
+    Page<Board> findBoardsByBoardTypeWithGrades(
+            @Param("boardType") String boardType,
+            @Param("grades") List<String> grades,
+            Pageable pageable
+    );
+
+    Page<Board> findAllByBoardTypeAndCategoryIdAndIsDeletedIsFalse(
+            String boardType,
+            Integer categoryId,
+            Pageable pageable
+    );
+
+    Page<Board> findAllByBoardTypeAndIsDeletedIsFalseAndCategoryIdAndTitleContainingIgnoreCase(
+            String boardType,
+            Integer categoryId,
+            String keyword,
+            Pageable pageable
+    );
+
+
 }
